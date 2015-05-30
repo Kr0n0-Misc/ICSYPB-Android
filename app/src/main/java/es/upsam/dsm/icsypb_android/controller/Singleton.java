c4529626b6582a7587da2a255011d66896020926
Carlos Crisóstomo Vals <ccvals@linux.com>
2015-05-30 22:05:13 +0200

    Creación de patrón Singleton base (Controller)

---
+++ app/src/main/java/es/upsam/dsm/icsypb_android/controller/Singleton.java
@@ -0,0 +1,33 @@
+package es.upsam.dsm.icsypb_android.controller;
+
+/**
+ * Singleton
+ *
+ * @brief Patrón singleton para gestionar los datos entre activities
+ * @author Kr0n0
+ */
+public class Singleton {
+    // Atributo de clase - Instancia
+    private static Singleton mInstance = null;
+
+    // Lista de atributos de clase a gestionar
+
+
+
+    private Singleton() {
+        // Inicializamos los atributos de clase
+
+    }
+
+    /**
+     * getInstance() - Devuelve la instancia del singleton
+     * @return mInstance
+     */
+    public static Singleton getInstance() {
+        if(mInstance == null)
+        {
+            mInstance = new Singleton();
+        }
+        return mInstance;
+    }
+}
