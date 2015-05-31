8785344b535ea2ab40d9669014e03eee5b07ebc7
Carlos Crisóstomo Vals <ccvals@linux.com>
2015-05-31 17:19:49 +0200

    Añadida Activity SplashScreen y configurada en el Manifest como inicio de aplicación

---
+++ app/src/main/java/es/upsam/dsm/icsypb_android/activities/SplashScreen.java
@@ -0,0 +1,17 @@
+package es.upsam.dsm.icsypb_android.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import es.upsam.dsm.icsypb_android.R;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
