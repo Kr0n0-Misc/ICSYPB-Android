aa0ce659963e8c3cca59543f34ab46a48331f079
Carlos Crisóstomo Vals <ccvals@linux.com>
2015-06-24 18:39:11 +0200

    Escaneo Bluetooth integrado en Activity

--- app/src/main/java/es/upsam/dsm/icsypb_android/activities/BTScanActivity.java
+++ app/src/main/java/es/upsam/dsm/icsypb_android/activities/BTScanActivity.java
@@ -1,8 +1,15 @@
 package es.upsam.dsm.icsypb_android.activities;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,30 +22,54 @@ import es.upsam.dsm.icsypb_android.R;
 import es.upsam.dsm.icsypb_android.controller.Singleton;
 import es.upsam.dsm.icsypb_android.entities.Baliza;
 
+
+/**
+ * BYScanActivity
+ *
+ *
+ *
+ * Referencias :    http://stackoverflow.com/questions/3285580/how-to-periodically-scan-for-bluetooth-devices-on-android?answertab=oldest#tab-top
+ *                  http://www.programcreek.com/java-api-examples/index.php?api=android.bluetooth.BluetoothAdapter
+ *
+ *
+ */
 public class BTScanActivity extends Activity {
     List<Baliza> lBalizas;
+    private BluetoothAdapter mBtAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         int posicion;
         final Button boton;
         final TextView texto;
+        final String mac_dispositivo;
 
-        // 1 - Visualizamos el activity
+        // 1 - Registramos el intent para bluetooth
+        IntentFilter intentFilter_reset = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(this.mReceiver_reset, intentFilter_reset);
+        IntentFilter intentFilter_scan = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(this.mReceiver_scan, intentFilter_scan);
+
+
+        // 2 - Inicializamos el dispositivo Bluetooth y recogemos la MAC
+        mBtAdapter=BluetoothAdapter.getDefaultAdapter();
+        mac_dispositivo = mBtAdapter.getAddress();
+        // 3 - Visualizamos el activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_btscan);
-        // 2 - Recogemos Singleton con el contexto de aplicación
+        // 4 - Recogemos Singleton con el contexto de aplicación
         final Singleton datos = Singleton.getInstance(this);
-        // 3 - Recogemos los parámetros desde la otra Activity
+        // 5 - Recogemos los parámetros desde la otra Activity
         //     RutasActivity(posicion) -> Posicion del array
         Bundle parametros = getIntent().getExtras();
         posicion = parametros.getInt("posicion");
-        // 4 - Recogemos la lista de Balizas de esa ruta
+        // 6 - Recogemos la lista de Balizas de esa ruta
         lBalizas = datos.getRuta(posicion).getBalizas();
-        // 5 - Recogemos el boton y el texto
+        // 7 - Recogemos el boton y el texto
         boton = (Button) findViewById(R.id.button);
         texto = (TextView) findViewById(R.id.textView2);
-        // 6 - Asociamos el método al botón
+        // 8 - Asociamos el método al botón
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,8 +85,9 @@ public class BTScanActivity extends Activity {
                 if (bTexto.equals("EMPEZAR")) {
                     //datos.escanearBT(this, lBalizas);
                     // TODO
-
+                    mBtAdapter.startDiscovery();
                     // Cambiamos el botón a PARAR
+                    texto.setText("Pulse el botón PARAR cuando termine");
                     boton.setText("PARAR");
                 }
 
@@ -69,11 +101,16 @@ public class BTScanActivity extends Activity {
                 - Deshabilitamos el botón hasta que el onClick del ListView lo active (si hay alguno seleccionado)
                  */
                 if (bTexto.equals("PARAR")) {
-                    datos.stopBTScan();
+                    // 1 - Deshabilitamos la búsqueda bluetooth
+                    if (mBtAdapter.isDiscovering())
+                        mBtAdapter.cancelDiscovery();
+                    // 2 - Desregistramos el BroadcastReceiver bluetooth
+                    getBaseContext().unregisterReceiver(mReceiver_reset);
+                    getBaseContext().unregisterReceiver(mReceiver_scan);
                     boton.setEnabled(false);
                     texto.setText("Seleccione los puntos y pulse guardar");
                     boton.setText("GUARDAR");
-                    //TODO
+                    //TODO PROPAGAR MAC
                 }
 
                 /* 3 - GUARDAR
@@ -89,4 +126,75 @@ public class BTScanActivity extends Activity {
             }
         });
     }
+
+    /**
+     *
+     * @param lBalizas
+     * @param cadena
+     * @return
+     */
+    public boolean buscarArray(List<Baliza> lBalizas, String cadena) {
+        boolean resultado = false;
+        String mac;
+
+        // 1 - Recorremos el ArrayList de lBalizas
+        for (int i=0;i<lBalizas.size();i++) {
+            mac = lBalizas.get(i).getMac();
+            if (mac.equalsIgnoreCase(cadena)) {
+                return (true);
+            }
+        }
+        return(resultado);
+    }
+
+
+
+
+    /**
+     *
+     */
+    public final BroadcastReceiver mReceiver_reset = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Log.v("[BTScan]", "Reiniciando discover");
+                mBtAdapter.startDiscovery();
+                }
+            }
+    };
+
+    /**
+     *
+     */
+    public final BroadcastReceiver mReceiver_scan = new BroadcastReceiver() {
+        Boolean encontrado = false;
+        String mac_actual;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            // Si se detecta un dispositivo
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mac_actual = device.getAddress();
+                encontrado = buscarArray(lBalizas, mac_actual);
+                if (encontrado = true) {
+                    Log.v("[BTScan]", "MAC ECONTRADA " + mac_actual);
+                    /* TODO Añadimos los campos al objeto Tracking
+                    String mac_usuario :: ownMAC;
+                    String mac_baliza :: mac_actual;
+                    String fecha_actual :: fecha_actual=sdf.format(date);;
+                    int id_baliza :: ;
+                    int id_ruta; */
+                }
+            }
+        }
+    };
+
 }
+
+
